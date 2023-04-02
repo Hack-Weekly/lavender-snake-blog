3f74e62b1d4b0038ef63ffe86f158b0f4071ec14
Ajoy Kumar Das <ajoykumardas08@gmail.com>
2023-04-02 11:32:43 +0530

    basic-layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main>
-      <div className="text-3xl font-bold text-purple-700">Lavender Snake Blog</div>
+      <div className="text-2xl">Blog posts</div>
     </main>
   )
 }
