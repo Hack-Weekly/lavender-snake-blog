66c334181e54dbc9fa643a4ab7990a44dce6ac73
tanerijun <tanerivince@gmail.com>
2023-04-02 18:34:54 +0800

    Add prettier-plugin-tailwindcss

--- app/Footer.tsx
+++ app/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
 	return (
-		<footer className="bg-violet-300 flex justify-center">
+		<footer className="flex justify-center bg-violet-300">
 			<p className=""> This is footer</p>
 		</footer>
 	)
