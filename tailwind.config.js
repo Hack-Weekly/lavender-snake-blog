2424cd92736cc47f2d0785e2dbaa19db2914f456
Xarunoba <me@xaru.win>
2023-04-02 14:04:28 +0800

    transferred the custom classes to not remove defaults

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,38 +11,39 @@ module.exports = {
     './src/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    colors: {
-      primary: {
-        DEFAULT: '#4F378B',
-        50: '#F0EBFF',
-        100: '#C8BBEA',
-        200: '#A692D1',
-        300: '#816EB3',
-        400: '#674D9B',
-        500: '#4F378B',
-        600: '#3E256F',
-        700: '#2D165B',
-        800: '#1E0D45',
-        900: '#11062B',
-        950: '#060011',
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: '#4F378B',
+          50: '#F0EBFF',
+          100: '#C8BBEA',
+          200: '#A692D1',
+          300: '#816EB3',
+          400: '#674D9B',
+          500: '#4F378B',
+          600: '#3E256F',
+          700: '#2D165B',
+          800: '#1E0D45',
+          900: '#11062B',
+          950: '#060011',
+        },
+        error: {
+          DEFAULT: '#8C1D18',
+          content: '#F9DEDC',
+          focus: '#601410',
+          'content-focus': '#F2B8B5',
+        },
+        success: {
+          DEFAULT: '#2B9115',
+          content: '#DFF6D9',
+          focus: '#1D620D',
+          'content-focus': '#B0E9A1',
+        },
       },
-      error: {
-        DEFAULT: '#8C1D18',
-        content: '#F9DEDC',
-        focus: '#601410',
-        'content-focus': '#F2B8B5',
-      },
-      success: {
-        DEFAULT: '#2B9115',
-        content: '#DFF6D9',
-        focus: '#1D620D',
-        'content-focus': '#B0E9A1',
+      fontFamily: {
+        serif: ['var(--font-serif)', ...defaultTheme.fontFamily.serif],
       },
     },
-    fontFamily: {
-      serif: ['var(--font-serif)', ...defaultTheme.fontFamily.serif],
-    },
-    extend: {},
   },
   plugins: [],
 }
