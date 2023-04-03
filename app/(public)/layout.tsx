36eb492847077180b0b34e651b94fedc458dfd99
Xarunoba <me@xaru.win>
2023-04-03 13:29:28 +0800

    changed <main> width to full

--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -9,7 +9,7 @@ export default function PublicLayout({
 	return (
 		<div className="min-h-dscreen flex flex-col items-center justify-between">
 			<Nav />
-			<main className="max-w-3xl">{children}</main>
+			<main className="w-full">{children}</main>
 			<Footer />
 		</div>
 	)
