0a1deaf9acd4570dc27ab3aa22edc6c5f75388db
Xarunoba <me@xaru.win>
2023-04-03 12:27:34 +0800

    adding test external url for images

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
 	experimental: {
 		appDir: true,
 	},
+	images: {
+		domains: ["https://picsum.photos/"], // Lorem Pictures. Replace this later on.
+	},
 }
 
 module.exports = nextConfig
