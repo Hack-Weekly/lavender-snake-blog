427ee765be23f70ef27704f0f5e5a2055f4b6caf
Xarunoba <me@xaru.win>
2023-04-03 13:29:53 +0800

    initial design for the post page (with mock data)

---
+++ app/(public)/blog/[slug]/page.tsx
@@ -0,0 +1,88 @@
+import Image from "next/image"
+import Link from "next/link"
+
+interface Post {
+	title: string
+	body: string
+	tags: string[]
+	author: string
+	imgsrc: string
+	datetime: number
+}
+
+// please delete later on
+const mockData = {
+	title: "The Science of Geometry",
+	body: `Geometry is a branch of mathematics that deals with the study of shapes, sizes, positions, and dimensions of objects in space. It is a fundamental field of study that has been in existence for thousands of years, dating back to ancient civilizations such as the Greeks, Egyptians, and Babylonians. The science of geometry has played a significant role in the development of various branches of science and engineering, including architecture, physics, and computer graphics. In this article, we will delve deeper into the fascinating world of geometry, exploring its history, concepts, and practical applications in modern-day society.
+
+	History of Geometry
+	
+	Geometry can be traced back to ancient Egypt, where it was used to survey and measure the land for the construction of the pyramids. The Egyptians were the first to use the concept of right angles, and they developed a system of measurement based on the length of the forearm, called the cubit. The Egyptians also used geometry to solve practical problems, such as calculating the area of fields and the volume of containers.
+	
+	The ancient Greeks are credited with the development of Euclidean geometry, which is the study of points, lines, angles, surfaces, and solids in space. Euclid, a Greek mathematician, wrote a book called "Elements," which is considered the most influential textbook in the history of mathematics. The book contains 13 volumes and contains the basic principles of geometry, including axioms, postulates, and theorems. The Elements remained the standard textbook on geometry for over 2000 years.
+	
+	In the Islamic Golden Age, Persian mathematician Al-Khwarizmi wrote a book called "The Compendious Book on Calculation by Completion and Balancing," which was a major contribution to the field of algebra and geometry. In the 17th century, French mathematician René Descartes developed analytic geometry, which is the study of geometry using algebraic equations. This allowed mathematicians to describe geometric shapes using equations, making it easier to solve geometric problems.
+	
+	Geometry Concepts
+	
+	Geometry is a vast field that includes many concepts and principles. Here are some of the fundamental concepts of geometry:
+	
+	Points: A point is a location in space that has no size or dimension. It is represented by a dot.
+	Lines: A line is a straight path that extends indefinitely in both directions. It is represented by a straight line with arrows on both ends.
+	Line segments: A line segment is a part of a line that has two endpoints.
+	Angles: An angle is formed by two rays that have a common endpoint. It is measured in degrees, with a full circle equal to 360 degrees.
+	Polygons: A polygon is a closed shape with straight sides. Examples include triangles, quadrilaterals, and pentagons.
+	Circles: A circle is a closed shape with all points equidistant from a center point. It is defined by its radius, which is the distance from the center point to any point on the circle.
+	
+	Practical Applications of Geometry
+	
+	Geometry has practical applications in many fields of science and engineering. Here are some examples of how geometry is used in real-world situations:
+	
+	Architecture: Architects use geometry to design and construct buildings. They use principles of geometry to calculate angles, lengths, and areas of different parts of a building. They also use geometric shapes to create aesthetically pleasing designs.
+	Art: Artists use geometry to create aesthetically pleasing designs. They use geometric shapes and patterns to create symmetry and balance in their art. Examples include the use of the golden ratio in paintings and sculptures.
+	Computer graphics: Computer graphics use geometry to create realistic images and animations. Geometric shapes and formulas are used to create 3D models of objects and scenes.
+	Engineering: Engineers use geometry to design and build structures such as bridges, tunnels, and roads. 
+	
+	In conclusion, geometry is a fascinating field of mathematics with a rich history and practical applications in many areas of science and engineering. From the ancient Egyptians to modern-day architects and engineers, geometry has been used to solve practical problems and create aesthetically pleasing designs. The concepts of points, lines, angles, and shapes are the building blocks of geometry, and they provide a framework for understanding the physical world around us. As technology continues to advance, the applications of geometry will continue to grow and evolve, and we can expect to see even more innovative and practical uses of this fundamental field of mathematics.`,
+	tags: ["mathematics", "science"],
+	author: "Jude",
+	imgsrc: "https://picsum.photos/1024", //1024 x 1024
+	datetime: Date.now(),
+} as Post
+
+export default function BlogPost({ params }: { params: { slug: string } }) {
+	const datetime = new Date(mockData.datetime).toLocaleDateString()
+	return (
+		<section className="flex w-full flex-col items-center">
+			<article className="flex max-w-4xl flex-col lg:w-[58%]">
+				{/* {params.slug} */}
+				<p className="mb-6 text-center text-xs font-light text-neutral-400">
+					{mockData.tags.join(", ").toUpperCase()}
+				</p>
+				<h1 className="mb-11 text-center text-3xl font-bold">
+					{mockData.title}
+				</h1>
+				<Image
+					className="mb-3.5 aspect-video"
+					src={mockData.imgsrc}
+					alt="Featured Image"
+					width={1024}
+					height={1024}
+				/>
+				<div className="gap-2. flex flex-row-reverse">
+					<p className="flex-none text-right text-sm">
+						Written by{" "}
+						<Link href="/blog?author=Jude" className="font-bold text-primary">
+							{mockData.author}
+						</Link>{" "}
+						on {datetime}
+					</p>
+					<p className="mb-14 pt-12">{mockData.body}</p>
+				</div>
+			</article>
+			<div className="w-full px-4 py-4 lg:px-20 lg:py-12">
+				<p className="text-2xl font-light text-neutral-700">COMMENTS</p>
+			</div>
+		</section>
+	)
+}
