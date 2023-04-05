622cc0858733c79200478d8504954259f746f3d7
tanerijun <tanerivince@gmail.com>
2023-04-05 23:18:49 +0800

    Fix: main content not taking full available height

--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -9,7 +9,7 @@ export default function PublicLayout({
 	return (
 		<div className="min-h-dscreen flex flex-col items-center justify-between">
 			<Nav />
-			<main className="w-full bg-primary-bg">{children}</main>
+			<main className="w-full flex-1 bg-primary-bg">{children}</main>
 			<Footer />
 		</div>
 	)
