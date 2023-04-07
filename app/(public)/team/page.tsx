cee61269d027cc306a49793b0aa781233255a164
tanerijun <tanerivince@gmail.com>
2023-04-07 16:31:15 +0800

    Improve styling

--- app/(public)/team/page.tsx
+++ app/(public)/team/page.tsx
@@ -2,7 +2,7 @@ import SnakeGame from "./SnakeGame"
 
 export default function Team() {
 	return (
-		<div>
+		<div className="align-center flex flex-1 justify-center">
 			<SnakeGame />
 		</div>
 	)
