fc7b238653811a8ac3725a73f9e7590107575eb6
Xarunoba <me@xaru.win>
2023-04-12 01:47:02 +0800

    add a check before reading file

--- app/(public)/[slug]/page.tsx
+++ app/(public)/[slug]/page.tsx
@@ -6,13 +6,18 @@ import Markdown from "markdown-to-jsx"
 import matter from "gray-matter"
 import getPostMetadata from "../components/getPostMetadata"
 import Tag from "../components/Tag"
+import { BsHourglassSplit } from "react-icons/bs"
 
 const getPostContent = (slug: string) => {
 	const folder = "posts/"
 	const file = `${folder}${slug}.md`
-	const content = fs.readFileSync(file, "utf8")
-	const matterResult = matter(content)
-	return matterResult
+	if (fs.existsSync(file)) {
+		const content = fs.readFileSync(file, "utf8")
+		const matterResult = matter(content)
+		return matterResult
+	} else {
+		return null
+	}
 }
 
 export async function generateStaticParams() {
@@ -20,18 +25,17 @@ export async function generateStaticParams() {
 	return posts.map((post) => ({
 		slug: post.slug,
 	}))
-};
+}
 
 export default function BlogPost({ params }: { params: { slug: string } }) {
 	const post = getPostContent(params.slug)
-
-	return (
+	return post ? (
 		<section className="flex w-full flex-col items-center">
 			<article className="mt-6 flex max-w-xl flex-col items-center space-y-12 text-black dark:text-[#c7cfd9] lg:w-[58%]">
 				<div className="flex gap-1 text-center text-sm font-light text-neutral-400">
 					{post.data.tags.map(
 						(tag: string, index: number, elements: string[]) => {
-							const isNext = elements[index + 1];
+							const isNext = elements[index + 1]
 							return (
 								<div key={tag}>
 									<Tag tag={tag} />
@@ -66,10 +70,15 @@ export default function BlogPost({ params }: { params: { slug: string } }) {
 
 				<Markdown className="prose dark:prose-invert">{post.content}</Markdown>
 			</article>
-			<div className="w-full px-4 py-4 lg:px-20 lg:py-12">
+			{/* <div className="w-full px-4 py-4 lg:px-20 lg:py-12">
 				<p className="mb-4 text-2xl font-light text-neutral-700">COMMENTS</p>
-				{/* <CommentEditor /> */}
-			</div>
+				<CommentEditor />
+			</div> */}
 		</section>
+	) : (
+		<div className="mx-5 mt-20 flex flex-col items-center justify-center gap-2 text-lg xs:flex-row">
+			<BsHourglassSplit />
+			We cannot find this post.
+		</div>
 	)
 }
