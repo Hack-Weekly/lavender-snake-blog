2acd7533cfa6e61cf6a65f6b814801b8507f322c
Xarunoba <me@xaru.win>
2023-04-13 02:31:46 +0800

    adjusted gap for smaller screen sizes

--- app/(public)/components/Nav.tsx
+++ app/(public)/components/Nav.tsx
@@ -1,15 +1,15 @@
-import Image from "next/image";
-import ThemeToggler from "./ThemeToggler";
-import Link from "next/link";
-import SearchBar from "./SearchBar";
-import getPostMetadata from "./getPostMetadata";
+import Image from "next/image"
+import ThemeToggler from "./ThemeToggler"
+import Link from "next/link"
+import SearchBar from "./SearchBar"
+import getPostMetadata from "./getPostMetadata"
 
 export default function Nav() {
-	const postMetadata = getPostMetadata();
+	const postMetadata = getPostMetadata()
 
 	return (
-		<nav className="flex w-full flex-row items-center justify-between bg-primary-bg px-2 py-0 text-primary dark:bg-[#0B0A1F] dark:text-primary-100 lg:px-8">
-			<Link href="/" className="flex flex-row items-center gap-1">
+		<nav className="flex w-full  items-center justify-between bg-primary-bg px-2 py-0 text-primary dark:bg-[#0B0A1F] dark:text-primary-100 lg:px-8">
+			<Link href="/" className="flex  items-center gap-1">
 				<Image
 					className="aspect-square"
 					src="/lavender-snake.png"
@@ -20,9 +20,12 @@ export default function Nav() {
 				/>
 				<div className="text-lg font-bold xs:text-xl">Snakebyte</div>
 			</Link>
-			<div className="flex flex-row items-center gap-1 xs:gap-2 sm:gap-4 md:gap-8 lg:gap-12 relative">
+			<div className="relative flex items-center gap-4 md:gap-8 lg:gap-12">
 				<SearchBar postMetadatas={postMetadata} />
-				<Link href="/team" className="flex text-center text-xs xs:text-sm sm:text-base font-bold">
+				<Link
+					href="/team"
+					className="flex text-center text-xs font-bold xs:text-sm sm:text-base"
+				>
 					<span className="min-[380px]:hidden">TEAM </span>
 					<span className="hidden min-[380px]:block">MEET THE TEAM </span>
 				</Link>
