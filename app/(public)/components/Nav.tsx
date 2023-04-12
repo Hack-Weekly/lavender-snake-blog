8b0a2a55e69d25f0895a5bd1225ff617a91a0c13
Xarunoba <me@xaru.win>
2023-04-12 14:52:22 +0800

    increased button gap for smaller screen

--- app/(public)/components/Nav.tsx
+++ app/(public)/components/Nav.tsx
@@ -4,8 +4,8 @@ import Link from "next/link"
 
 export default function Nav() {
 	return (
-		<nav className="flex w-full flex-row items-center justify-between px-2 py-0 lg:px-8 bg-primary-bg dark:bg-[#0B0A1F] text-primary dark:text-primary-100">
-			<Link href="/" className="flex flex-row gap-1 items-center">
+		<nav className="flex w-full flex-row items-center justify-between bg-primary-bg px-2 py-0 text-primary dark:bg-[#0B0A1F] dark:text-primary-100 lg:px-8">
+			<Link href="/" className="flex flex-row items-center gap-1">
 				<Image
 					className="aspect-square"
 					src="/lavender-snake.png"
@@ -14,10 +14,13 @@ export default function Nav() {
 					height={64}
 					priority
 				/>
-				<div className="text-lg xs:text-xl font-bold">Snakebyte</div>
+				<div className="text-lg font-bold xs:text-xl">Snakebyte</div>
 			</Link>
-			<div className="flex flex-row items-center gap-1 sm:gap-2 md:gap-8 lg:gap-16">
-				<Link href="/team" className="text-center text-xs xs:text-sm sm:text-base font-bold">
+			<div className="flex flex-row items-center gap-8 lg:gap-16">
+				<Link
+					href="/team"
+					className="text-center text-xs font-bold xs:text-sm sm:text-base"
+				>
 					MEET THE TEAM
 				</Link>
 				<ThemeToggler />
