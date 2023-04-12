5a9eee32af2915552ec7b144e6347e5e4052516d
Xarunoba <me@xaru.win>
2023-04-13 04:45:56 +0800

    styled category list

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -1,39 +1,38 @@
 import FeaturedPost from "./components/FeaturedPost"
 import FilteredPosts from "./components/FilteredPosts"
 import PostCard from "./components/PostCard"
-import getPostMetadata from "./components/getPostMetadata";
-import getAllUniqueTags from "./components/getAllUniqueTags";
-import Link from "next/link";
+import getPostMetadata from "./components/getPostMetadata"
+import getAllUniqueTags from "./components/getAllUniqueTags"
+import Link from "next/link"
 
 export default function Home() {
-	const allTags = getAllUniqueTags();
+	const allTags = getAllUniqueTags()
 
-	const postMetadata = getPostMetadata();
+	const postMetadata = getPostMetadata()
 	// console.log(postMetadata);
-	
+
 	return (
 		<div className="flex flex-col items-center">
 			<FeaturedPost />
-			<div className="flex items-center justify-between w-full h-12 mt-4 px-5 border-y-4 border-black bg-[#E9E9D5] dark:bg-[#293548]">
-				<div className="hidden sm:flex overflow-hidden">
-					<div className="font-medium mr-4">BLOG CATEGORIES</div>
-					<div className="flex items-center gap-3 text-sm text-primary-800 dark:text-primary-50/80 overflow-hidden">
-						{allTags.map((tag:string) => {
-							return (
-								<Link href={`/category/${tag}`} key={tag} className="hover:text-primary-300">
-									{tag.toUpperCase()}
-								</Link>
-							)
-						})}
-					</div>
+			<div className="mt-10 flex w-screen items-center gap-4 overflow-hidden px-5">
+				<div className="whitespace-nowrap font-medium">CATEGORIES</div>
+				<div className="flex items-center gap-3 overflow-auto text-sm text-primary-50/80 scrollbar-none">
+					{allTags.map((tag: string) => {
+						return (
+							<Link
+								href={`/category/${tag}`}
+								key={tag}
+								className="rounded-md px-4 py-1 odd:bg-primary-400 even:bg-secondary-400 hover:text-primary-300"
+							>
+								{tag.toUpperCase()}
+							</Link>
+						)
+					})}
 				</div>
-				<div className="sm:hidden font-medium mr-4">BLOG POSTS</div>
 			</div>
-			<FilteredPosts > 
+			<FilteredPosts>
 				{postMetadata.map((post, index) => {
-					return(
-						<PostCard key={index} cardData={post} />
-					)
+					return <PostCard key={index} cardData={post} />
 				})}
 			</FilteredPosts>
 		</div>
