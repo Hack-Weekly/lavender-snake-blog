e5ef546b5a6eab1b33946c16da2ccb73bd9408b4
Xarunoba <me@xaru.win>
2023-04-12 13:36:00 +0800

    fixed mobile overflow issue for the post content

--- app/(public)/[slug]/page.tsx
+++ app/(public)/[slug]/page.tsx
@@ -35,9 +35,9 @@ export default function BlogPost({ params }: { params: { slug: string } }) {
 	const words = plainText.trim().split(/\s+/).length
 	const readingTime = Math.ceil(words / wpm)
 	return post ? (
-		<section className="flex w-full flex-col items-center p-4">
+		<section className="flex flex-col items-center">
 			<article className="my-6 flex max-w-xl flex-col items-center space-y-12 text-black dark:text-[#c7cfd9] lg:w-[58%] ">
-				<div className="flex gap-1 text-center text-sm font-light text-neutral-400">
+				<div className="flex gap-1 px-4 text-center text-sm font-light text-neutral-400">
 					{post.data.tags.map(
 						(tag: string, index: number, elements: string[]) => {
 							const isNext = elements[index + 1]
@@ -50,17 +50,19 @@ export default function BlogPost({ params }: { params: { slug: string } }) {
 						}
 					)}
 				</div>
-				<h1 className="text-center text-3xl font-bold dark:text-white">
+				<h1 className="px-4 text-center text-3xl font-bold dark:text-white">
 					{post.data.title}
 				</h1>
-				<Image
-					className="aspect-video rounded-md"
-					src={post.data.imageSrc}
-					alt="Featured Image"
-					width={600}
-					height={600}
-				/>
-				<div className="flex w-full flex-row items-center justify-between gap-2">
+				<div className="px-4">
+					<Image
+						className="aspect-video rounded-md bg-neutral-200 object-contain dark:bg-neutral-800"
+						src={post.data.imageSrc}
+						alt="Featured Image"
+						width={600}
+						height={600}
+					/>
+				</div>
+				<div className="flex w-full flex-row items-center justify-between gap-2 px-4">
 					<p className="text-left text-sm font-light text-neutral-400">
 						{readingTime} {readingTime != 1 ? "minutes" : "minute"} read...
 					</p>
@@ -68,7 +70,7 @@ export default function BlogPost({ params }: { params: { slug: string } }) {
 						Written by{" "}
 						<Link
 							href={`/author/${post.data.author}`}
-							className="font-bold text-primary"
+							className="font-bold text-primary-800 dark:text-primary-100"
 						>
 							{post.data.author}
 						</Link>{" "}
@@ -76,7 +78,7 @@ export default function BlogPost({ params }: { params: { slug: string } }) {
 					</p>
 				</div>
 
-				<Markdown className="prose max-w-none dark:prose-invert">
+				<Markdown className="prose w-screen min-w-0 max-w-none px-4 dark:prose-invert">
 					{post.content}
 				</Markdown>
 			</article>
